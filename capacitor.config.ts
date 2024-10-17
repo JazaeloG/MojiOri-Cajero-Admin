351f8544455e556cac25cb191b9eeae0e7bf6217
JazaeloG <epicgamesbbass@gmail.com>
2024-10-17 08:51:06 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'MojiOri-Cajero',
+  webDir: 'www'
+};
+
+export default config;
